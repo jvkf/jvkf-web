dbcaf92ef429bf53caba71d7cab5dbcdcd7e6a1d
João Vítor Ferraz <git@p1pa.dev>
2024-10-16 15:36:14 -0300

    feat: reset default settings, add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Rubik } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const rubik = Rubik({
+  subsets: ['latin'],
+  display: 'swap',
+});
