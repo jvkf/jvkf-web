c5db4468cd5744f7d117e951d240bdfb371077d1
João Vítor Ferraz <git@p1pa.dev>
2024-10-17 22:02:20 -0300

    feat: improve nav-switch (sr-only and hover)

--- app/ui/navigation/navigation-switch-btn.tsx
+++ app/ui/navigation/navigation-switch-btn.tsx
@@ -3,7 +3,7 @@
 import { Locale, usePathname, useRouter } from '@/i18n/routing';
 import clsx from 'clsx';
 import { Languages } from 'lucide-react';
-import { useLocale } from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import { useTransition } from 'react';
 
 export default function NavigationSwitchBtn() {
@@ -12,6 +12,7 @@ export default function NavigationSwitchBtn() {
   const pathname = usePathname();
   const locale = useLocale() as Locale;
   const nextLocale = locale === 'en' ? 'pt' : 'en';
+  const t = useTranslations('Switch');
 
   const onClick = () => {
     startTransition(() => {
@@ -23,13 +24,13 @@ export default function NavigationSwitchBtn() {
     <button
       onClick={onClick}
       className={clsx([
-        'bg-lava-red text-black flex items-center gap-2 px-4 py-2 rounded-md border-solid border-gray-200 border',
+        'bg-[#FD5F62] hover:bg-lava-red duration-300 ease-in transition-colors text-black flex items-center gap-2 px-4 py-1.5 rounded-md border-solid border-gray-200 border text-sm',
         isPending && 'opacity-30',
       ])}
       disabled={isPending}
     >
-      <Languages size={20} />
-      <p className="sr-only">Click to change to language:</p>
+      <Languages size={16} />
+      <p className="sr-only">{t('sr-text')}</p>
       {nextLocale}
     </button>
   );
