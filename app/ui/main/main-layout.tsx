beaa103f3d75a4c8f573234c1f924f33d9361be7
João Vítor Ferraz <git@p1pa.dev>
2024-10-18 13:47:06 -0300

    feat: add animation

--- app/ui/main/main-layout.tsx
+++ app/ui/main/main-layout.tsx
@@ -8,7 +8,12 @@ type Props = {
 
 export default function MainLayout({ AsideComponent, TextComponent }: Props) {
   return (
-    <main className="md:grid md:grid-cols-3 md:gap-5 my-auto px-4 md:px-0">
+    <main
+      className={clsx(
+        'md:grid md:grid-cols-3 md:gap-5 my-auto px-4 md:px-0 md:pb-10',
+        'fadeInFromBottom'
+      )}
+    >
       <article
         className={clsx(
           'md:col-span-1 bg-lava-red bg-opacity-[0.075] rounded-sm w-full flex',
