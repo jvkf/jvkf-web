803a2caf37417166c350bec1a959df8ba65623be
João Vítor Ferraz <jvkferraz@gmail.com>
2025-10-05 16:38:38 -0300

    feat: implement middleware to cv url

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,23 @@
 import createMiddleware from 'next-intl/middleware';
+import { NextRequest, NextResponse } from 'next/server';
 import { routing } from './i18n/routing';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+const CV_REDIRECTS: Record<string, string> = {
+  '/pt-cv.pdf': 'https://drive.google.com/file/d/1y92m5SnYa-AI30zZpu0RhG3UYZSjyDSD/view?usp=sharing',
+  '/english-cv.pdf': 'https://drive.google.com/file/d/14aX-EhgWvhyhSQ0tlu71oc08sKjlFvzC/view?usp=sharing',
+};
+
+export default function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (CV_REDIRECTS[pathname]) {
+    return NextResponse.redirect(CV_REDIRECTS[pathname], 301);
+  }
+  return intlMiddleware(request);
+}
 
 export const config = {
-  matcher: ['/', '/(en|pt)/:path*'],
+  matcher: ['/', '/(en|pt)/:path*', '/pt-cv.pdf', '/english-cv.pdf'],
 };
